a03d6a063aa8d72f9b6074d2612c354ca8ce9b53
Zach Cobell <zachary.cobell@arcadis.com>
2017-06-23 00:09:11 -0600

    Adjusting cronJob.sh

--- cronJob.sh
+++ cronJob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-if ps -ef | grep -v grep | grep -v cronJob.sh  | grep SumpMonitor ; then
+n=$(ps -ef | grep -v grep | grep -v cronJob.sh  | grep SumpMonitor | wc -l)
+if [ $n == 1 ] ; then
     exit 0
 else
     echo "Sump Monitor restarted at " $(date)
