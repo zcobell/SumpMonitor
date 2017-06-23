b96fe21d4d978abf5228b99cecbecd1f9b057356
Zach Cobell <zachary.cobell@arcadis.com>
2017-06-22 20:43:45 -0600

    Adjusting cron script

--- cronJob.sh
+++ cronJob.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-args="-c -i 30 -a 10 -p -n"
-
-if ps -ef | grep -v grep | grep SumpMonitor ; then
+if ps -ef | grep -v grep | grep -v cronJob.sh  | grep SumpMonitor ; then
     exit 0
 else
     echo "Sump Monitor restarted at " $(date)
-    /home/pi/sumpMonitor/SumpMonitor/SumpMonitor $(args) >> sumpLog.log &
+    /home/pi/sumpMonitor/SumpMonitor/SumpMonitor -c -i 30 -p -n >> sumpLog.log &
 fi
