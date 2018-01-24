9b27a98e7769adfdfa91aa72e7cf07baf6bd4bb4
Zach Cobell <zachary.cobell@arcadis.com>
2018-01-24 00:24:16 -0700

    Adding licensing information

--- src/pins.h
+++ src/pins.h
@@ -1,3 +1,21 @@
+//------------------------------GPLv3------------------------------------//
+//
+//  This file is part of SumpMonitor.
+//
+//  SumpMonitor is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  SumpMonitor is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with SumpMonitor. If not, see <http://www.gnu.org/licenses/>.
+//
+//------------------------------GPLv3------------------------------------//
 #ifndef PINS_H
 #define PINS_H
 
